6719d9d9f44b9303331d82b8e52b2758d8f9fa12
西风逍遥游 <sun_xfancy@163.com>
2017-10-11 23:44:13 -0700

    update JOB_PATH

--- qsub.sh
+++ qsub.sh
@@ -1,7 +1,7 @@
 
-cp -r Project\ 1/ Project1-gcc5.1
-cp -r Project\ 1/ Project1-gcc4.7
-cp -r Project\ 1/ Project1-gcc4.4
+cp -r Project\ 1/ Project1-gcc5.1/
+cp -r Project\ 1/ Project1-gcc4.7/
+cp -r Project\ 1/ Project1-gcc4.4/
 
 cd Project1-gcc5.1/
 module purge
@@ -18,7 +18,7 @@ module purge
 module load gcc-4.7.1
 gcc -v
 make
-qsub jobfile3
+qsub jobfile4
 cd ..
 
 
@@ -26,7 +26,7 @@ cd Project1-gcc4.4/
 gcc -v
 module purge
 make
-qsub jobfile4
+qsub jobfile3
 cd ..
 
 echo finished
