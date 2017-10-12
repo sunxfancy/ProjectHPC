2f6e9e61c11060ffc490960f6375f9c8e561abee
西风逍遥游 <sun_xfancy@163.com>
2017-10-11 23:40:11 -0700

    fix gcc version

--- qsub.sh
+++ qsub.sh
@@ -4,6 +4,9 @@ cp -r Project\ 1/ Project1-gcc4.7
 cp -r Project\ 1/ Project1-gcc4.4
 
 cd Project1-gcc5.1/
+module purge
+module load gcc-5.1.0
+gcc -v
 make
 qsub jobfile1
 qsub jobfile2
@@ -11,12 +14,17 @@ cd ..
 
 
 cd Project1-gcc4.7/
+module purge
+module load gcc-4.7.1
+gcc -v
 make
 qsub jobfile3
 cd ..
 
 
 cd Project1-gcc4.4/
+gcc -v
+module purge
 make
 qsub jobfile4
 cd ..
